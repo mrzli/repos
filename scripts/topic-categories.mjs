10e2c615e3f9327265373b3c69ee886b8bd08ca5
Goran Mržljak <goran.mrzljak@gmail.com>
2026-07-26 15:50:53 +0200

    update topics

--- scripts/topic-categories.mjs
+++ scripts/topic-categories.mjs
@@ -1,28 +1,6 @@
 export const TOPIC_CATEGORIES = new Map([
-  ['docs-generic', { order: 1, primary: 'Docs', secondary: 'Generic' }],
-  [
-    'docs-development',
-    { order: 2, primary: 'Docs', secondary: 'Development' },
-  ],
-  [
-    'libs-shared-util',
-    { order: 3, primary: 'Libs', secondary: 'Shared Util' },
-  ],
-  [
-    'libs-browser-util',
-    { order: 4, primary: 'Libs', secondary: 'Browser Util' },
-  ],
-  ['libs-node-util', { order: 5, primary: 'Libs', secondary: 'Node Util' }],
-  [
-    'libs-development-util',
-    { order: 6, primary: 'Libs', secondary: 'Development Util' },
-  ],
-  ['libs-test-util', { order: 7, primary: 'Libs', secondary: 'Test Util' }],
-  ['tools', { order: 8, primary: 'Tools', secondary: null }],
-  ['setup', { order: 9, primary: 'Setup', secondary: null }],
-  ['template', { order: 10, primary: 'Templates', secondary: null }],
-  ['sites', { order: 11, primary: 'Sites', secondary: null }],
-  ['trading', { order: 12, primary: 'Trading', secondary: null }],
-  ['problems', { order: 13, primary: 'Problems', secondary: null }],
-  ['example', { order: 14, primary: 'Example', secondary: null }],
+  ['docs', { order: 1, primary: 'Docs', secondary: null }],
+  ['setup', { order: 2, primary: 'Setup', secondary: null }],
+  ['template', { order: 3, primary: 'Templates', secondary: null }],
+  ['sites', { order: 4, primary: 'Sites', secondary: null }]
 ]);
